cd3f2e07b1a5e72a946f22712ccbcd612bfc4158
DJIOTSA <mhulodjiotsa.com>
2024-12-26 22:16:45 +0100

    add myWork folder with starting code

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,12 +1,37 @@
-import Starter from "./tutorial/01-useState/starter/01-error-example";
-import Finale from "./tutorial/01-useState/final/01-error-example";
+
+import Style from "./Style";
 
 function App() {
   return (
-    <div className="container">
-      <Starter />
-      <Finale />
-    </div>
+    <main className="container">
+      {/* title */}
+      <h1>Advanced React Concepts:</h1>
+      <h4>
+        <ul>
+          <li>Hooks</li>
+          <li>Forms</li>
+          <li>Context API</li>
+          <li>Prop drilling</li>
+          <li>Custom Hooks</li>
+          <li>performance</li>
+          <li>etc</li>
+        </ul>
+      </h4>
+
+      <hr style={Style.hrH2} />
+
+      {/* Hooks */}
+      <h2>1. Hooks</h2>
+      <hr style={Style.hrH3}></hr>
+
+      {/* useState */}
+      <section>
+        <h3>1.1. useState</h3>
+
+        
+        <hr style={Style.hrH3}></hr>
+      </section>
+    </main>
   );
 }
 
---
+++ 03-advanced-react/src/Style.jsx
@@ -0,0 +1,12 @@
+const Style ={
+    hrH2:{
+        border: '0.5rem solid blue',
+        margin: '3rem 0'
+    },
+    hrH3: {
+        border: '1px dotted red',
+        margin: '2rem 0'
+    }
+}
+
+export default Style;
