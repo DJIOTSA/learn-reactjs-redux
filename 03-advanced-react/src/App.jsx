dd9861dbf8d2ddd731856a8d9d42e3ba9f2b6d85
DJIOTSA <mhulodjiotsa.com>
2024-12-26 21:23:44 +0100

    update 03-advanced-react app.jsx

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import ErrorExample from "./tutorial/01-useState/final/01-error-example";
-import ErrorExampleStart from "./tutorial/01-useState/starter/01-error-example";
+import Starter from "./tutorial/01-useState/starter/01-error-example";
+import Finale from "./tutorial/01-useState/final/01-error-example";
 
 function App() {
   return (
-    <div className='container'>
-      {/* <ErrorExample /> */}
-      <ErrorExampleStart />
+    <div className="container">
+      <Starter />
+      <Finale />
     </div>
   );
 }
